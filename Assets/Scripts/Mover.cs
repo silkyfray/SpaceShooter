9fdb44540bba8c4e99b3f6ec70e904ac20a9857e
Antonio Nikolov <anikolovn@gmail.com>
2015-11-07 09:25:41 +0000

    Tutorial 6

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody rigidBody;
+    private Transform transform;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    rigidBody = GetComponent<Rigidbody>();
+	    transform = GetComponent<Transform>();
+        rigidBody.velocity = transform.forward * speed;
+	}
+
+}
