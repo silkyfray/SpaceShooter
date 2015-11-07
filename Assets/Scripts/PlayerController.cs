45b6e2984ec6e44cc38d087e8748d37fde4b7cdc
Antonio Nikolov <anikolovn@gmail.com>
2015-11-07 00:00:39 +0000

    Tutorial 5

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+};
+
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
+
+    private Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        
+        rigidBody.velocity = movement * speed;
+
+        rigidBody.position = new Vector3
+            (
+                Mathf.Clamp(rigidBody.position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp(rigidBody.position.z, boundary.zMin, boundary.zMax)
+            );
+
+        rigidBody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidBody.velocity.x *-tilt);
+    }
+}
