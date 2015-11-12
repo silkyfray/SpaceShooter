ec6a844e997e0e82723b57d0edaf442207e6fedf
Antonio Nikolov <anikolovn@gmail.com>
2015-11-12 21:46:19 +0000

    Tutorial 9

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
+
+    private Rigidbody rigidBody;
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.angularVelocity = Random.insideUnitSphere * tumble;
+
+    }
+
+}
