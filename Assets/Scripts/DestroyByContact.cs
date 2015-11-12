2a17d6a1bc27d8b78383d7183a2a9e95756c2b69
Antonio Nikolov <anikolovn@gmail.com>
2015-11-12 22:11:16 +0000

    Tutorial 10

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DestroyByContact : MonoBehaviour {
+public class DestroyByContact : MonoBehaviour
+{
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+    private Transform transform;
+
+    public void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
@@ -11,5 +21,15 @@ public class DestroyByContact : MonoBehaviour {
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
+
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+            Transform playerTransform = other.GetComponent<Transform>();
+
+            Instantiate(playerExplosion, playerTransform.position, playerTransform.rotation);
+
+        }
     }
 }
